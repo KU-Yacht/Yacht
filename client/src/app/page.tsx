b0f2d503e60e89f6a7701ed46d805d07f49b6c6c
MinboyKim <kdm3801@gmail.com>
2024-10-02 14:37:27 +0900

    feat: Header
    Implement Header

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ui/mode-toggle";
-
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold">Hello, world!</h1>
-      <Button>Click</Button>
-      <ModeToggle />
-    </div>
-  );
+  return <div className="p-8">body</div>;
 }
