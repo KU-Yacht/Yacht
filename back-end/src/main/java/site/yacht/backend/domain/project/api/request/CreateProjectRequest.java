6777ef834993ebc4afc1c0a98bcede6685f83905
seungtaekhong <hsk4991149@naver.com>
2024-10-05 01:13:07 +0900

    feat: modify project name max length

--- back-end/src/main/java/site/yacht/backend/domain/project/api/request/CreateProjectRequest.java
+++ back-end/src/main/java/site/yacht/backend/domain/project/api/request/CreateProjectRequest.java
@@ -15,9 +15,9 @@ import org.hibernate.validator.constraints.Length;
 public class CreateProjectRequest {
 
     @NotBlank(message = "프로젝트 이름은 필수값입니다.")
-    @Length(min = 2, max = 10, message = "프로젝트 이름은 2글자 이상 10글자 이하여야 합니다.")
+    @Length(min = 2, max = 20, message = "프로젝트 이름은 2글자 이상 20글자 이하여야 합니다.")
     @Pattern(regexp = "^[0-9a-zA-Z가-힣]*$", message = "프로젝트 이름은 한글, 영어, 숫자만 허용됩니다.")
-    @Schema(description = "사용할 프로젝트 이름(한글, 숫자, 영어 사용가능)", example = "myProject", minLength = 2, maxLength = 10, requiredMode = RequiredMode.REQUIRED)
+    @Schema(description = "사용할 프로젝트 이름(한글, 숫자, 영어 사용가능)", example = "myProject", minLength = 2, maxLength = 20, requiredMode = RequiredMode.REQUIRED)
     private String projectName;
 
 }
