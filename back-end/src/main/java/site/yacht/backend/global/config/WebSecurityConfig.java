f9064bf980c7d2f1bd79c736645cb3fc7cab22a1
seungtaekhong <hsk4991149@naver.com>
2024-10-04 18:34:23 +0900

    feat: CORS Allow

--- back-end/src/main/java/site/yacht/backend/global/config/WebSecurityConfig.java
+++ back-end/src/main/java/site/yacht/backend/global/config/WebSecurityConfig.java
@@ -15,9 +15,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import site.yacht.backend.domain.authentication.service.TokenManager;
 import site.yacht.backend.global.security.JwtAuthenticationFilter;
 import site.yacht.backend.global.security.UserDetailsServiceImpl;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -55,6 +60,21 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("HEAD", "POST", "GET", "DELETE", "PUT"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
