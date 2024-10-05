7bb950d8d3ab7895e09fcf045af80e65314e22fc
seungtaekhong <hsk4991149@naver.com>
2024-10-05 10:57:47 +0900

    doc: modify swagger example

--- back-end/src/main/java/site/yacht/backend/domain/user/api/request/UserLoginRequest.java
+++ back-end/src/main/java/site/yacht/backend/domain/user/api/request/UserLoginRequest.java
@@ -16,12 +16,12 @@ public class UserLoginRequest {
 
     @NotBlank(message = "이메일은 필수값입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
-    @Schema(description = "이메일", example = "tester@naver.com", requiredMode = RequiredMode.REQUIRED)
+    @Schema(description = "이메일", example = "admin@test.com", requiredMode = RequiredMode.REQUIRED)
     private String email;
 
     @NotBlank(message = "비밀번호는 필수값입니다.")
     @Length(min = 4, max = 16, message = "비밀번호는 4글자 이상 16글자 이하여야 합니다.")
-    @Schema(description = "비밀번호", example = "q1w2e3r4", minLength = 4, maxLength = 16, requiredMode = RequiredMode.REQUIRED)
+    @Schema(description = "비밀번호", example = "admin", minLength = 4, maxLength = 16, requiredMode = RequiredMode.REQUIRED)
     private String password;
 
 }
