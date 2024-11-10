d366d0eaf7c69a21ad020d41f566f638cde657d2
seungtaekhong <hsk4991149@naver.com>
2024-11-10 23:55:08 +0900

    feat: deploy to cloud

--- back-end/src/main/java/site/yacht/backend/domain/deployment/service/DeployService.java
+++ back-end/src/main/java/site/yacht/backend/domain/deployment/service/DeployService.java
@@ -3,20 +3,29 @@ package site.yacht.backend.domain.deployment.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import site.yacht.backend.domain.application.domain.Application;
 import site.yacht.backend.domain.application.exception.ApplicationNotFoundException;
 import site.yacht.backend.domain.application.repository.ApplicationRepository;
 import site.yacht.backend.domain.deployment.dto.DeployApplicationRequestToCloud;
 
+import javax.net.ssl.*;
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class DeployService {
 
     @Value("${cloud.url}")
@@ -41,12 +50,64 @@ public class DeployService {
             return false;
         }
 
-        RestTemplate restTemplate = new RestTemplate();
+        return deployToCloud(jsonString) == HttpStatus.OK.value();
+    }
 
-        HttpEntity<String> cloudRequest = new HttpEntity<>(jsonString);
-        ResponseEntity<Void> response = restTemplate.exchange(cloudUrl, HttpMethod.POST, cloudRequest, Void.class);
-        System.out.println(response.getStatusCode());
+    private int deployToCloud(String body) {
+        disableSslVerification();
+        try {
+            URL url = new URL(cloudUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            connection.setDoOutput(true);
 
-        return response.getStatusCode() == HttpStatus.OK;
+            try (OutputStream os = new BufferedOutputStream(connection.getOutputStream())) {
+                byte[] input = body.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            return connection.getResponseCode();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("POST request failed with exception: " + e.getMessage());
+        }
+    }
+
+    private void disableSslVerification() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 }
