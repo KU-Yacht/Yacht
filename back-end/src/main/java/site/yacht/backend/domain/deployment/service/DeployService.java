a783ee59236554cabf7dc56ee78979301413bfcc
seungtaekhong <hsk4991149@naver.com>
2024-11-10 22:27:43 +0900

    feat: deploy to cloud

--- back-end/src/main/java/site/yacht/backend/domain/deployment/service/DeployService.java
+++ back-end/src/main/java/site/yacht/backend/domain/deployment/service/DeployService.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import site.yacht.backend.domain.application.domain.Application;
 import site.yacht.backend.domain.application.exception.ApplicationNotFoundException;
 import site.yacht.backend.domain.application.repository.ApplicationRepository;
@@ -23,24 +28,25 @@ public class DeployService {
     public boolean deploy(Long userId, Long applicationId) {
         Application application = applicationRepository.findById(applicationId)
                 .orElseThrow(ApplicationNotFoundException::new);
+
         // validation permit
 
         DeployApplicationRequestToCloud requestToCloud = new DeployApplicationRequestToCloud(application);
+        String jsonString = "";
         try {
             // 객체를 JSON 문자열로 변환
-            String jsonString = objectMapper.writeValueAsString(requestToCloud);
+            jsonString = objectMapper.writeValueAsString(requestToCloud);
             System.out.println("JSON 형식으로 변환된 데이터: " + jsonString);
         } catch (JsonProcessingException e) {
             return false;
         }
 
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        HttpEntity<String> cloudRequest = new HttpEntity<>("body");
-//        ResponseEntity<Void> response = restTemplate.exchange(cloudUrl, HttpMethod.POST, cloudRequest, Void.class);
-//        System.out.println(response.getStatusCode());
-//
-//        return response.getStatusCode() == HttpStatus.OK;
-        return true;
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<String> cloudRequest = new HttpEntity<>(jsonString);
+        ResponseEntity<Void> response = restTemplate.exchange(cloudUrl, HttpMethod.POST, cloudRequest, Void.class);
+        System.out.println(response.getStatusCode());
+
+        return response.getStatusCode() == HttpStatus.OK;
     }
 }
