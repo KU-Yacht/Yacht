e3017a22cbf21387d0cff7313af34ffbc0edd82f
seungtaekhong <hsk4991149@naver.com>
2024-11-10 09:00:12 +0900

    feat: 배포 API 변경

--- back-end/src/main/java/site/yacht/backend/domain/deployment/api/DeploymentApi.java
+++ back-end/src/main/java/site/yacht/backend/domain/deployment/api/DeploymentApi.java
@@ -3,14 +3,15 @@ package site.yacht.backend.domain.deployment.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import site.yacht.backend.domain.application.domain.Application;
 import site.yacht.backend.domain.application.exception.ApplicationNotFoundException;
 import site.yacht.backend.domain.application.repository.ApplicationRepository;
-import site.yacht.backend.domain.deployment.api.request.DeployApplicationRequest;
 import site.yacht.backend.domain.deployment.api.response.DeployDetailResponse;
 import site.yacht.backend.domain.deployment.service.DeployService;
+import site.yacht.backend.global.security.UserDetailsImpl;
 
 @RestController
 @RequestMapping("api/deployment")
@@ -24,17 +25,18 @@ public class DeploymentApi {
     @GetMapping("/{applicationId}")
     @Operation(summary = "애플리케이션 배포 정보 조회", description = "애플리케이션의 배포 정보를 조회합니다.")
     @Transactional
-    public DeployDetailResponse findApplicationDetail(@PathVariable Long applicationId) {
+    public DeployDetailResponse findDeploymentDetail(@PathVariable Long applicationId) {
         Application application = applicationRepository.findById(applicationId)
                 .orElseThrow(ApplicationNotFoundException::new);
 
         return new DeployDetailResponse(application);
     }
 
-    @PostMapping
-    @Operation(summary = "애플리케이션 배포하기", description = "애플리케이션을 배포합니다..")
-    public String deployApplication(@RequestBody DeployApplicationRequest request) {
-        boolean deploy = deployService.deploy(request);
+    @PostMapping("/{applicationId}")
+    @Operation(summary = "애플리케이션 배포하기", description = "애플리케이션을 배포합니다.")
+    public String deployApplication(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                    @PathVariable Long applicationId) {
+        boolean deploy = deployService.deploy(userDetails.user().getId(), applicationId);
         if (deploy) {
             return "{\"isSuccess\": true}";
         }
--- back-end/src/main/java/site/yacht/backend/domain/deployment/service/DeployService.java
+++ back-end/src/main/java/site/yacht/backend/domain/deployment/service/DeployService.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import site.yacht.backend.domain.deployment.api.request.DeployApplicationRequest;
+import site.yacht.backend.domain.application.domain.Application;
+import site.yacht.backend.domain.application.exception.ApplicationNotFoundException;
+import site.yacht.backend.domain.application.repository.ApplicationRepository;
+import site.yacht.backend.domain.deployment.dto.DeployApplicationRequestToCloud;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +18,17 @@ public class DeployService {
     private String cloudUrl;
 
     private final ObjectMapper objectMapper;
+    private final ApplicationRepository applicationRepository;
 
-    public boolean deploy(DeployApplicationRequest request) {
-        // validation
+    public boolean deploy(Long userId, Long applicationId) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(ApplicationNotFoundException::new);
+        // validation permit
 
+        DeployApplicationRequestToCloud requestToCloud = new DeployApplicationRequestToCloud(application);
         try {
             // 객체를 JSON 문자열로 변환
-            String jsonString = objectMapper.writeValueAsString(request);
+            String jsonString = objectMapper.writeValueAsString(requestToCloud);
             System.out.println("JSON 형식으로 변환된 데이터: " + jsonString);
         } catch (JsonProcessingException e) {
             return false;
